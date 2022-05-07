c9fefa9c3174900ed17985d0e8bec2928deac8e3
alexis <itss04@pandero.com.pe>
2022-05-07 13:01:49 -0500

    cambios2

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,6 +4,18 @@ class Conexion{
 
 	static public function conectar(){
 
+		$link = new PDO("mysql:host=db07.cvmhnpwmiojs.us-east-1.rds.amazonaws.com;dbname=db07",
+			            "admin",
+			            "adminadmin");
+
+		$link->exec("set names utf8");
+
+		return $link;
+
+	}
+
+	static public function conectar2(){
+
 		$link = new PDO("mysql:host=localhost;dbname=sis_inventario",
 			            "root",
 			            "");
